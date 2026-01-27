c2f9046df9aa779bc2780f8109221fc669debdd2
Daniel Lim <daniel2060306@gmail.com>
2026-01-27 00:48:57 -0500

    Redesign navbar as vertical sidebar with animations

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
 
 const Navbar = () => {
   const location = useLocation();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Trigger animation on mount
+    setIsLoaded(true);
+  }, []);
 
   return (
     <nav className="navbar">
-      <div className="navbar-menu">
+      <div className={`navbar-menu ${isLoaded ? 'loaded' : ''}`}>
         <Link 
           to="/" 
           className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`}
